ef25d530c297fcecb5a94c93a54f06e878039ee8
lily23445 <kalash.p017@gmail.com>
2025-05-04 12:44:15 +0530

    Testing

--- demo_project/urls.py
+++ demo_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('meditrack/', include('demo_app.urls')),  # Include your app's URLs
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
 urlpatterns += static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
