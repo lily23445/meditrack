afebc7d2698988be38e0d7c221f13be0ab750840
lily23445 <kalash.p017@gmail.com>
2025-05-04 12:56:27 +0530

    Githib config

--- demo_project/urls.py
+++ demo_project/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     path('meditrack/', include('demo_app.urls')),  # Include your app's URLs
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
 urlpatterns += static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
